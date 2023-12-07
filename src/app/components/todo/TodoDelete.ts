ac7abc61adc9e68e2b6f59f296f3f884df114659
imahoritatsuki <imahori0611@gmail.com>
2023-12-08 00:16:47 +0900

    initial commit

---
+++ src/app/components/todo/TodoDelete.ts
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default async function TodoDelete(id:string) {
+  await fetch(`http://localhost:3000/todos/${id}`, {
+    method: 'DELETE',
+  }); 
+}
+
---
+++ src/app/todo/page.tsx
@@ -0,0 +1,12 @@
+import TodoList from '../components/todo/TodoList';
+
+function Page() {
+  return (
+    <div>
+      <h1>Todos</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default Page;
